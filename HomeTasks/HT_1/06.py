28f9d1c01f0ecdffb646b0cc55a9ec9e1db35493
AlexNizovoy <axel.aka.alex@gmail.com>
2017-10-20 22:59:15 +0300

    HomeTask #1. Task #6 complete.

--- HomeTasks/HT_1/06.py
+++ HomeTasks/HT_1/06.py
@@ -1,14 +1,37 @@
-# 5. Write a script to convert decimal to hexadecimal
-#         Sample decimal number: 30, 4
-#         Expected output: 1e, 04
+# 6. Write a script to check whether a specified value is contained in a group of values.
+#         Test Data :
+#         3 -> [1, 5, 8, 3] : True
+#         -1 -> (1, 5, 8, 3) : False
 
-n = input("Enter an integer to convert from decimal to hexadecimal: ")
-if not len(n):
-    n = 0
+values_list = input("#1 Enter a space-separated group of values (leave it blank for default value): ")
+if not len(values_list):
+    values_list = "1 5 8 3".split(" ")
+else:
+    values_list = values_list.split(" ")
+
+# run down for values_list, delete empty elements, try convert first into int, then into float. If fail - keep str.
+for i in range(len(values_list)-1, -1, -1):
+    if values_list[i] == "":
+        del values_list[i]
+    else:
+        try:
+            values_list[i] = int(values_list[i])
+        except:
+            try:
+                values_list[i] = float(values_list[i])
+            except:
+                pass
+
+chk = input("Enter a value to find in group: ")
+if not len(chk):
+    chk = 0
 else:
     try:
-        n = int(n)
+        chk = int(chk)
     except:
-        print("You enter not integer!")
-        exit(1)
-print("Decimal {0}.\nHexadecimal {1}".format(n, hex(n)))
+        try:
+            chk = float(chk)
+        except:
+            pass
+
+print("{0} --> {1} : {2}".format(chk, values_list, chk in values_list))
