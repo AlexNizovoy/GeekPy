53030fd79ed37c354ab88f6cb597dfe0d041f0df
AlexNizovoy <axel.aka.alex@gmail.com>
2017-10-25 17:44:52 +0300

    HomeTaks #2. Task#1 Func#1 complete.

---
+++ HomeTasks/HT_2/ht_2.py
@@ -0,0 +1,97 @@
+# 1. (таких ф-цiй потрiбно написати 3 -> рiзними варiантами) Написати функцiю season, приймаючу 1 аргумент — номер мiсяця (вiд 1 до 12), яка буде повертати пору року, якiй цей мiсяць належить (зима, весна, лiто або осiнь).
+
+
+def task_1():
+
+    def season_1(month):
+        """season_1(...)
+                season_1(month)
+
+                Return the season to whitch the entered month belongs"""
+        season = (month % 12) // 3
+        return {
+            0: "зима",
+            1: "весна",
+            2: "лiто",
+            3: "осiнь",
+        }.get(season, "Щось не так :(")
+
+    def season_2():
+        pass
+
+    def season_3():
+        pass
+
+    print("\nНаписати функцiю season, приймаючу 1 аргумент - номер мiсяця (вiд 1 до 12), яка буде повертати пору року, якiй цей мiсяць належить (зима, весна, лiто або осiнь).")
+    print("(таких ф-цiй потрiбно написати 3 -> рiзними варiантами)")
+    while True:
+        n = input("Введiть номер функцiї (пустий рядок для повернення в головне меню): ")
+        if not len(n):
+            return None
+        try:
+            n = int(n)
+        except:
+            print("Невiрне значення номеру завдання!")
+            continue
+        func = {
+            1: season_1,
+            2: season_2,
+            3: season_3
+        }.get(n, print)
+        month = input("Введiть номер мiсяця: ")
+        if not len(month):
+            return None
+        try:
+            month = int(month)
+        except:
+            print("Невiрне значення мiсяця!")
+            continue
+        print("Мiсяць №{} - це {}.\n".format(month, func(month)))
+
+
+def task_2():
+    pass
+
+
+def task_3():
+    pass
+
+
+def task_4():
+    pass
+
+
+def task_5():
+    pass
+
+
+def task_6():
+    pass
+
+
+def task_7():
+    pass
+
+
+if __name__ == '__main__':
+    print("HomeTask #2. Alex Nizovoy.\n")
+    while True:
+        n = input("Введiть номер завдання (пустий рядок для виходу): ")
+        if not len(n):
+            print("На все добре!")
+            exit(0)
+        try:
+            n = int(n)
+        except:
+            print("Невiрне значення номеру завдання!")
+            continue
+        task = {
+            1: task_1,
+            2: task_2,
+            3: task_3,
+            4: task_4,
+            5: task_5,
+            6: task_6,
+            7: task_7
+        }.get(n, print)
+        task()
