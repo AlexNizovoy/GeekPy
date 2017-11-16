60cb86126cc82b2366dfc531f7ed0c1c2bc191c7
AlexNizovoy <axel.aka.alex@gmail.com>
2017-11-16 17:10:43 +0200

    HomeTask #5. Initial commit.

--- HomeTasks/HT_4/testing.py
+++ HomeTasks/HT_4/testing.py
@@ -1,12 +1,14 @@
 import re
 
-regex = r"^\d{4}-\d{2}-\d{2}\s\d{,2}:\d{2}:\d{2}[.,]\d{3}\s\d+\s(WARNING|ERROR|CRITICAL).+$"
-eol = r"\n"
 
-test_str = open("test.log").read()
+def test1():
+    regex = r"^\d{4}-\d{2}-\d{2}\s\d{,2}:\d{2}:\d{2}[.,]\d{3}\s\d+\s(WARNING|ERROR|CRITICAL).+$"
+    eol = r"\n"
 
-matches = re.finditer(regex, test_str, re.MULTILINE)
-line_numbers = tuple(item.start() for item in re.finditer(eol, test_str))
+    test_str = open("test.log").read()
 
-for match in matches:
-    print("Match was found at line #{line} ({start}-{end}): {match}".format(line=line_numbers.index(match.end()) + 1, start=match.start(), end=match.end(), match=match.group()))
+    matches = re.finditer(regex, test_str, re.MULTILINE)
+    line_numbers = tuple(item.start() for item in re.finditer(eol, test_str))
+
+    for match in matches:
+        print("Match was found at line #{line} ({start}-{end}): {match}".format(line=line_numbers.index(match.end()) + 1, start=match.start(), end=match.end(), match=match.group()))
